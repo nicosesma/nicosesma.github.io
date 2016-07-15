90671622f222bba91fe014cae701eb893c92478e
nicosesma <nicosm310@gmail.com>
2016-07-15 11:51:16 -0700

    Created a javascript function to open/close nav menu on mobile screens with the open and close buttons, made those buttons not display on regular screens and created the nav menu for mobiles a div, with the class name of menu so we had to change the inline class links, of a, to be direct descendants using the > on css

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav(){
+  var menu = document.querySelector('.menu');
+  menu.classList.add('menu-open')
+}
+
+function closeNav(){
+  var menu = document.querySelector('.menu');
+  menu.classList.remove('menu-open')
+}
